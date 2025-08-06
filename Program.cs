3e412f0dfcb75dc427865e0c42eed923b9a38b10
Marina Santos Morais <marinasmorais07@gmail.com>
2025-08-06 18:43:54 -0300

    Desafio Concluídogit status

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+//instanciando um objeto da classe NOKIA
+Nokia nokia1 = new("270307","Nokia +14","IMEI785469",25);
+nokia1.ReceberLigacao();
+nokia1.Ligar();
+nokia1.InstalarAplicativo("boomberman");
+
+Console.WriteLine( );
+//instanciando um objeto da classe SMARTPHONE
+
+Iphone iphone1 = new("852147", "Iphone B12", "IMEI5784613", 80);
+iphone1.ReceberLigacao();
+iphone1.Ligar();
+iphone1.InstalarAplicativo("Doulingo");
